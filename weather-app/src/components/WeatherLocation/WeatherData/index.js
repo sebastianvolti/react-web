26a902e95bf3ab51905d04c445dd9091be97b4ba
Sebastian Volti <sebavolti@gmail.com>
2020-04-15 12:53:23 -0300

    Changes in css and prop types validator

--- weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
+import propTypes from 'prop-types';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
-import WeatherIcons from 'react-weathericons';
-
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY
-} from '../../../constants/weathers'
 import './styles.css';
 
-const WeatherData = () => (
-    <div className="weatherDataCont">
-        <WeatherTemperature temperature={18}
-                            weatherState={RAIN}
+
+const WeatherData = ({data : {temperature,weatherState,humidity,wind}}) => {
+    return( <div className="weatherDataCont">
+        <WeatherTemperature temperature={temperature}
+                            weatherState={weatherState}
         />
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"} ></WeatherExtraInfo>
-    </div>
-);
+        <WeatherExtraInfo humidity={humidity} wind={wind} ></WeatherExtraInfo>
+    </div>);
+    
+};
+
+WeatherData.propTypes = {
+    data: propTypes.shape({
+        temperature: propTypes.number.isRequired,
+        weatherState: propTypes.string.isRequired,
+        humidity: propTypes.number.isRequired,
+        wind: propTypes.string.isRequired,
+    }),
+};
 
 export default WeatherData;
--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -3,11 +3,25 @@ import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY
+} from '../../constants/weathers'
 
+const data = {
+    temperature: 5,
+    weatherState: RAIN,
+    humidity: 10,
+    wind: '10 m/s',
+}
 const WeatherLocation = () => (
     <div className="weatherLocationCont">
         <Location city={"Montevideo,Uy"}></Location> 
-        <WeatherData></WeatherData>
+        <WeatherData data={data}></WeatherData>
     </div>
 );
 
