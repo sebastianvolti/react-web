badced6bb2c8c3a11ad55e6ddc2106b0a83a70c0
Sebastian Volti <sebavolti@gmail.com>
2020-05-21 16:19:58 -0300

    add redux-form

--- customers-app/customers-app/src/components/CustomerEdit.js
+++ customers-app/customers-app/src/components/CustomerEdit.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
 
 const CustomerEdit = ({name, ci, age}) => {
     return (
         <div>
             <h2>Edición del cliente</h2>
+            <form action="">
+                <div>
+                    <label htmlFor="name">Nombre: </label>
+                    <Field name="name" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="ci">CI: </label>
+                    <Field name="ci" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Edad: </label>
+                    <Field name="age" component="input" type="number"></Field>
+                </div>
+            </form>
             <h3>Nombre:{name} / CI:{ci} / Edad:{age} </h3>
         </div>
     );
@@ -16,4 +31,4 @@ CustomerEdit.propTypes = {
     age: propTypes.number,
 };
 
-export default CustomerEdit;
+export default reduxForm({ form: 'CustomerEdit'})(CustomerEdit);
